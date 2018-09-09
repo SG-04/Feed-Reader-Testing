7ba82a00563a6859c6533055b917114ec324b27a
Shruti Gupta <shrutigupta.sg33@gmail.com>
2018-09-09 17:33:13 +0530

    Test that loops through each feed to ensure that Name is not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -43,6 +43,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('Name is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                feedName = feed.name;
+                expect(feedName).toBeDefined();
+                expect(feedName.length).not.toBe(0);
+            });
+        });
     });
 
 
